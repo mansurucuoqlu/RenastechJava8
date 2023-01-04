eede501a3db18a06e47c353bed74da47794207e4
mansurucuoqlu <121526908+mansurucuoqlu@users.noreply.github.com>
2023-01-03 17:37:37 -0800

    joo

---
+++ Topic7_HA_IfElseIFCont.java
@@ -0,0 +1,19 @@
+package Day4_IfStatements;
+
+public class Topic7_HA_IfElseIFCont {
+    public static void main(String[] args) {
+        int x = 99;
+        if (x == 10) {
+            System.out.println("x is equal to 10");
+
+        } else if (x == 15) {
+            System.out.println( "x is equal to 15");}
+        else if ( x == 20){
+            System.out.println("x is equal to 20");
+
+        }
+        else {
+            System.out.println("x is somethging else");}
+
+        }
+    }

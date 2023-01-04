f74c5437da1b51d6a585f452cd689a77775a2ba9
mansurucuoqlu <121526908+mansurucuoqlu@users.noreply.github.com>
2023-01-03 17:38:51 -0800

    joo

---
+++ Topic1_HA_IfStatement.java
@@ -0,0 +1,7 @@
+package Day4_IfStatements;
+
+public class Topic1_HA_IfStatement {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Topic6_HA_IfElseIF.java
@@ -0,0 +1,26 @@
+package Day4_IfStatements;
+
+public class Topic6_HA_IfElseIF {
+    public static void main(String[] args) {
+        // a number can be positive negative or equal to 0
+        /*
+        if check if its positive say it is positigve
+        else if (check if its negative
+        else if equal to 0
+         */
+        int number = 10;
+        if (number > 0){
+            System.out.println("its poisitive");
+        }
+        else if (number < 0 ){
+            System.out.println("its negative");
+        }
+        else {
+            System.out.println("its 0");
+        }
+        System.out.println("End of the  code");
+
+
+
+    }
+}
